e8428af38522e0b2183ad128cedb7cddf6de0b47
JulianC3 <114505507+JulianC07@users.noreply.github.com>
2023-02-14 17:18:52 +0000

    Test change

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
-
+ 
 </script>
 
 <main>
   <!-- Replace this with your actual content to make a project :) -->
+ Hello world
 </main>
 
 <style>
   main {
-    
+    background-color: #0033a0;
+    color: white;
+    font-size: 14pt;
   }
   
 </style>

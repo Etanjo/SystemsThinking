756257c6dea844d265880808154d8b67c23b2f53
Etanjo <99751108+Etanjo@users.noreply.github.com>
2023-02-16 15:55:02 +0000

    Added border to Pattern

--- src/Pattern.svelte
+++ src/Pattern.svelte
@@ -1,16 +1,32 @@
 <script>
   
 </script>
-<div id = 'trapezoid'>AAAAAAHH</div>
+<div id = 'trapezoid'>
+  I Keep Getting Super Duper Scared, for no reason!!!!!
+  
+  
+</div>
 
 <style>
-  #trapezoid {
+  #trapezoid { /*Gotten from https://css-tricks.com/the-shapes-of-css/ */
       border-bottom: 100px solid #D7F6FE;
       border-left: 25px solid transparent;
       border-right: 25px solid transparent;
       height: 0;
       width: 100px;
-    color: #303633;
-    font-family: 'PT Sans'
+      color: #303633;
+      font-family: 'PT Sans';
+      position: relative;;
     }
+  #trapezoid::after{
+    content: ' ';
+    display: block;
+    position: absolute;
+    height: 5px;
+    width: 150px;
+    position: absolute;
+    top: 98px;
+    background-color: #033860;
+    left:-25px;
+  }
 </style>

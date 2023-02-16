9cabf68913ee57dd261ac243cff896fedcc65f40
Jack Pennie <90012417+Jack1234567891011121314@users.noreply.github.com>
2023-02-16 15:36:48 +0000

    made the component that puts the event on the iceberg

---
+++ src/Event.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Popup from './Popup.svelte'
+  export let popUpText
+  let open = false
+</script>
+<div>
+  <p>
+    <!--<slot/>-->
+    This is text
+  </p>
+  {#if open}
+    <Popup><p>{popUpText}</p></Popup>
+  {/if}
+</div>
+<style>
+  p{
+    color: #303633;
+  }
+</style>

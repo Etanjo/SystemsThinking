af59a2c1f527edaa69d107fd400a3f2d512451c4
Jack Pennie <90012417+Jack1234567891011121314@users.noreply.github.com>
2023-02-17 17:19:20 +0000

    got the top of the iceberg made and aligned the text inside properly

--- src/Event.svelte
+++ src/Event.svelte
@@ -3,18 +3,38 @@
   export let popUpText
   let open = false
 </script>
-<div id='triangle'>
-  <p>
-    <!--<slot/>-->
-    This is text
-  </p>
-  {#if open}
-    <Popup><p>{popUpText}</p></Popup>
-  {/if}
+<div class="wrap">
+  <div id='triangle'>
+  </div>
+  <div class="text">
+    <p>
+      <!--<slot/>-->
+      This is text
+    </p>
+    {#if open}
+      <Popup><p>{popUpText}</p></Popup>
+    {/if}
+  </div>
 </div>
 <style>
+  .wrap{
+    display: grid;
+    place-content: center;
+    position:relative;
+    height:100px;
+  }
   #triangle{
-    
+    border-top-color: transparent;
+    border-left: 50px solid transparent;
+    border-right: 50px solid transparent;
+    border-bottom: 100px solid #D7F6FE;
+    position: absolute;
+  }
+  .text{
+    position:relative;
+    text-align: center;
+    margin-top: calc(100px*0.325);
+    width: 50px;
   }
   p{
     color: #303633;

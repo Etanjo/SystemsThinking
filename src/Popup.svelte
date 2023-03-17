6bfe1e2e50980969a654572d746aa1fa44abf086
Etanjo <99751108+Etanjo@users.noreply.github.com>
2023-03-17 15:07:09 +0000

    POPUPS!!!!!

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,10 +1,10 @@
 <script>
 export let open = false
-
+export let text = ""
 </script>
 
 <main>
-   <div class:open> Pop up</div>
+   <div class:open>{text}</div>
 </main>
 
 <style>
@@ -12,7 +12,19 @@ export let open = false
    opacity: 0;
    pointer-events: none;
    transition: all 200ms;
+   width: 500px;
+   height: 80px;
+   overflow-y: auto;
+   color: #303633;
+   border: 2px solid #033860;
+   border-radius: 3px;
+   background-color: #D7F6FE;
+   padding: 1px;
+   
  }
+  div::-webkit-scrollbar-track{
+    background: #D7F6FE;
+  }
   div.open{
     opacity: 1;
     pointer-events: all;

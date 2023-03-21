c7e853ab12d25a368ffd999ebde7e561b5f9c0cb
Jack Pennie <90012417+Jack1234567891011121314@users.noreply.github.com>
2023-03-21 18:44:24 +0000

    disabled example

--- src/Tabs.svelte
+++ src/Tabs.svelte
@@ -6,7 +6,6 @@
 
 <Tab title = "display" top = 775 left = 767 active = {active=="display"} on:click={()=>{onChangeTab("display")}}/>
 <Tab title = "edit" top = 775 left = 900 active = {active=="edit"} on:click={()=>{onChangeTab("edit")}}/>
-<Tab title = "example" top = 775 left = 1034 active = {active=="example"} on:click={()=>{onChangeTab("example")}}/>
 
 
 <style>
